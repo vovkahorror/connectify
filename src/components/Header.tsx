0613c553d2e579a1e636d6115141b67088f6845a
Volodymyr Yaremchak <vovkahorror@gmail.com>
2022-09-21 21:26:21 +0300

    add basic components

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <header className={'header'}>
+      <img className={'header__img'}
+           src="https://oir.mobi/uploads/posts/2021-05/1620644854_26-oir_mobi-p-malenkii-morskoi-kotik-zhivotnie-krasivo-f-27.jpg"
+           alt=""/>
+    </header>
+  )
+}
---
+++ src/components/Nav.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Nav = () => {
+  return (
+    <nav className={'nav'}>
+      <ul>
+        <li><a href="">Profile</a></li>
+        <li><a href="">Messages</a></li>
+        <li><a href="">News</a></li>
+        <li><a href="">Music</a></li>
+        <li><a href="">Settings</a></li>
+      </ul>
+    </nav>
+  )
+}
