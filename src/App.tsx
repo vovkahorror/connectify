b33e8a50f3612d27b4370660aca2b5ccebb3e67d
Volodymyr Yaremchak <vovkahorror@gmail.com>
2022-09-29 22:26:26 +0300

    move BrowserRouter to index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import {Header} from "./components/Header/Header";
 import {Navbar} from "./components/Navbar/Navbar";
 import {Profile} from "./components/Profile/Profile";
 import {Dialogs, DialogsDataType, MessagesDataType} from "./components/Dialogs/Dialogs";
-import {BrowserRouter, Route} from "react-router-dom";
+import {Route} from "react-router-dom";
 import {News} from "./components/News/News";
 import {Music} from "./components/Music/Music";
 import {Settings} from "./components/Settings/Settings";
@@ -35,19 +35,17 @@ function App(props: AppPropsType) {
   }
 
   return (
-    <BrowserRouter>
-      <div className={'app-wrapper'}>
-        <Header/>
-        <Navbar/>
-        <div className={'app-wrapper__content'}>
-          <Route path={'/profile'} render={PostsRender}/>
-          <Route path={'/dialogs'} render={DialogsRender}/>
-          <Route path={'/news'} render={News}/>
-          <Route path={'/music'} render={Music}/>
-          <Route path={'/settings'} render={Settings}/>
-        </div>
+    <div className={'app-wrapper'}>
+      <Header/>
+      <Navbar/>
+      <div className={'app-wrapper__content'}>
+        <Route path={'/profile'} render={PostsRender}/>
+        <Route path={'/dialogs'} render={DialogsRender}/>
+        <Route path={'/news'} render={News}/>
+        <Route path={'/music'} render={Music}/>
+        <Route path={'/settings'} render={Settings}/>
       </div>
-    </BrowserRouter>
+    </div>
   );
 };
 
