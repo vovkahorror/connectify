9ea496d577b605c38790d19c5fb18b449a7c7fbb
Volodymyr Yaremchak <vovkahorror@gmail.com>
2022-11-10 01:16:01 +0200

    refactored Users.tsx

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import React from 'react';
 import {UserDataType} from "../../redux/users-reducer";
 import styles from './Users.module.css';
-import userPhoto from '../../assets/images/user.png'
+import userPhoto from '../../assets/images/user.png';
 
 type UsersPropsType = {
     users: UserDataType[],
@@ -12,19 +12,23 @@ type UsersPropsType = {
 }
 
 export const Users = (props: UsersPropsType) => {
-    if (props.users.length === 0) {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-            props.setUsers(response.data.items);
-        });
-    }
+    const getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+                props.setUsers(response.data.items);
+            });
+        }
+    };
 
     return (
         <div>
+            <button onClick={getUsers}>Get Users</button>
             {props.users.map(user => {
                 return (
                     <div key={user.id}>
                         <span>
-                            <div><img src={user.photos.small ? user.photos.small : userPhoto} className={styles.userPhoto}/></div>
+                            <div><img src={user.photos.large ? user.photos.large : userPhoto}
+                                      className={styles.userPhoto}/></div>
                             <div>
                                 {user.followed ? <button onClick={() => props.unfollow(user.id)}>Unfollow</button> :
                                     <button onClick={() => props.follow(user.id)}>Follow</button>}
