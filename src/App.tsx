9bc84c5449429c0ef36778bc1c428a0def8c85d7
Volodymyr Yaremchak <vovkahorror@gmail.com>
2023-04-09 19:48:09 +0300

    refactor: App to class component

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,40 @@
-import React from "react";
-import "./normalize.css";
-import "./App.css";
-import {Navbar} from "./components/Navbar/Navbar";
-import {Route} from "react-router-dom";
-import {News} from "./components/News/News";
-import {Music} from "./components/Music/Music";
-import {Settings} from "./components/Settings/Settings";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
-import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
-import HeaderContainer from "./components/Header/HeaderContainer";
-import Login from "./components/Login/Login";
+import React from 'react';
+import './normalize.css';
+import './App.css';
+import {Navbar} from './components/Navbar/Navbar';
+import {Route} from 'react-router-dom';
+import {News} from './components/News/News';
+import {Music} from './components/Music/Music';
+import {Settings} from './components/Settings/Settings';
+import DialogsContainer from './components/Dialogs/DialogsContainer';
+import UsersContainer from './components/Users/UsersContainer';
+import ProfileContainer from './components/Profile/ProfileContainer';
+import HeaderContainer from './components/Header/HeaderContainer';
+import Login from './components/Login/Login';
 
-function App() {
-    const ProfileRender = () => <ProfileContainer/>;
-    const DialogsRender = () => <DialogsContainer/>;
-    const UsersRender = () => <UsersContainer/>;
-    const LoginRender = () => <Login/>;
+class App extends React.Component {
+    render() {
+        const ProfileRender = () => <ProfileContainer/>;
+        const DialogsRender = () => <DialogsContainer/>;
+        const UsersRender = () => <UsersContainer/>;
+        const LoginRender = () => <Login/>;
 
-    return (
-        <div className={'app-wrapper'}>
-            <HeaderContainer/>
-            <Navbar/>
-            <div className={'app-wrapper__content'}>
-                <Route path={'/profile/:userID?'} render={ProfileRender}/>
-                <Route path={'/dialogs'} render={DialogsRender}/>
-                <Route path={'/users'} render={UsersRender}/>
-                <Route path={'/login'} render={LoginRender}/>
-                <Route path={'/news'} render={News}/>
-                <Route path={'/music'} render={Music}/>
-                <Route path={'/settings'} render={Settings}/>
+        return (
+            <div className={'app-wrapper'}>
+                <HeaderContainer/>
+                <Navbar/>
+                <div className={'app-wrapper__content'}>
+                    <Route path={'/profile/:userID?'} render={ProfileRender}/>
+                    <Route path={'/dialogs'} render={DialogsRender}/>
+                    <Route path={'/users'} render={UsersRender}/>
+                    <Route path={'/login'} render={LoginRender}/>
+                    <Route path={'/news'} render={News}/>
+                    <Route path={'/music'} render={Music}/>
+                    <Route path={'/settings'} render={Settings}/>
+                </div>
             </div>
-        </div>
-    );
-};
+        );
+    }
+}
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './index.css';
-import ReactDOM from "react-dom";
-import {BrowserRouter} from "react-router-dom";
-import App from "./App";
+import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
+import App from './App';
 import {store} from './redux/redux-store';
 import {Provider} from 'react-redux';
+
 
 ReactDOM.render(
     <BrowserRouter>
