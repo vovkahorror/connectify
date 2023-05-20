58652255e63580895406ed1c0ee9049a9282bfe6
Volodymyr Yaremchak <vovkahorror@gmail.com>
2023-05-20 03:32:04 +0300

    feat: started stylish the project and move to sass

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import styles from './Header.module.css';
+import styles from './Header.module.scss';
 import {NavLink} from 'react-router-dom';
+import logo from '../../assets/images/logo.svg';
 
 type HeaderPropsType = {
     isAuth: boolean;
@@ -11,9 +12,13 @@ type HeaderPropsType = {
 export const Header = (props: HeaderPropsType) => {
     return (
         <header className={styles.header}>
-            <img className={styles.header__img}
+            {/*<img className={styles.header__img}
                  src="https://oir.mobi/uploads/posts/2021-05/1620644854_26-oir_mobi-p-malenkii-morskoi-kotik-zhivotnie-krasivo-f-27.jpg"
-                 alt=""/>
+                 alt=""/>*/}
+            <NavLink className={styles.header__title} to="/">
+                <img className={styles.logo} src={logo} alt=""/>
+                <span>Connectify</span>
+            </NavLink>
             <div className={styles.loginBlock}>
                 {props.isAuth
                     ? <div>{props.login}
