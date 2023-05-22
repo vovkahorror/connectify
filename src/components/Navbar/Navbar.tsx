87206b28bd87adc0187e80e4de194c17bfe07d01
Volodymyr Yaremchak <vovkahorror@gmail.com>
2023-05-22 03:47:43 +0300

    feat: changed navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,22 +1,49 @@
 import React from 'react';
-import styles from './Navbar.module.css';
+import styles from './Navbar.module.scss';
 import {NavLink} from 'react-router-dom';
+import {ReactComponent as ProfileIcon} from '../../assets/icons/profile.svg';
+import {ReactComponent as MessageIcon} from '../../assets/icons/message.svg';
+import {ReactComponent as UsersIcon} from '../../assets/icons/users.svg';
+import {ReactComponent as ExploreIcon} from '../../assets/icons/explore.svg';
 
 export const Navbar = () => {
     return (
         <nav className={styles.nav}>
-            <ul>
-                <li className={styles.nav__item}><NavLink to="/profile" activeClassName={styles.active}
-                                                          exact>Profile</NavLink></li>
-                <li className={styles.nav__item}><NavLink to="/dialogs"
-                                                          activeClassName={styles.active}>Messages</NavLink></li>
-                <li className={styles.nav__item}><NavLink to="/users" activeClassName={styles.active}>Users</NavLink>
+            <ul className={styles.nav__list}>
+                <li className={styles.nav__item}>
+                    <NavLink to="/profile" className={styles.nav__link} activeClassName={styles.active} exact>
+                        <ProfileIcon className={styles.icon}/>
+                        <span>Profile</span>
+                    </NavLink>
                 </li>
-                <li className={styles.nav__item}><NavLink to="/news" activeClassName={styles.active}>News</NavLink></li>
-                <li className={styles.nav__item}><NavLink to="/music" activeClassName={styles.active}>Music</NavLink>
+                <li className={styles.nav__item}>
+                    <NavLink to="/dialogs" className={styles.nav__link} activeClassName={styles.active}>
+                        <MessageIcon className={styles.icon}/>
+                        <span>Messages</span>
+                    </NavLink>
                 </li>
-                <li className={styles.nav__item}><NavLink to="/settings"
-                                                          activeClassName={styles.active}>Settings</NavLink></li>
+                <li className={styles.nav__item}>
+                    <NavLink to="/users" className={styles.nav__link} activeClassName={styles.active}>
+                        <UsersIcon className={styles.icon}/>
+                        <span>Users</span>
+                    </NavLink>
+                </li>
+                <li className={styles.nav__item}>
+                    <NavLink to="/news" className={styles.nav__link} activeClassName={styles.active}>
+                        <ExploreIcon className={styles.icon}/>
+                        <span>News</span>
+                    </NavLink>
+                </li>
+                <li className={styles.nav__item}>
+                    <NavLink to="/music" className={styles.nav__link} activeClassName={styles.active}>
+                        <span>Music</span>
+                    </NavLink>
+                </li>
+                <li className={styles.nav__item}>
+                    <NavLink to="/settings" className={styles.nav__link} activeClassName={styles.active}>
+                        <span>Settings</span>
+                    </NavLink>
+                </li>
             </ul>
         </nav>
     );
