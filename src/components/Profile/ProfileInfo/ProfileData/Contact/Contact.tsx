014ae0c54d7bbafc1046bbac1bd91dc7cbd0e988
Volodymyr Yaremchak <vovkahorror@gmail.com>
2023-05-23 04:00:57 +0300

    feat: changed the Contact component

--- src/components/Profile/ProfileInfo/ProfileData/Contact/Contact.tsx
+++ src/components/Profile/ProfileInfo/ProfileData/Contact/Contact.tsx
@@ -2,7 +2,7 @@ import React, {ReactNode} from 'react';
 
 export const Contact = ({contactIcon, contactTitle, contactValue}: ContactPropsType) => {
     return (
-        <li>{contactIcon}{contactTitle}: {contactValue}</li>
+        <li><a href={contactValue} target={'_blank'} rel="noreferrer">{contactIcon} {contactTitle}</a></li>
     );
 };
 
