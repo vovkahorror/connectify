b31411a4f7e340119dd221a71754510fc1470880
Volodymyr Yaremchak <vovkahorror@gmail.com>
2023-05-31 22:54:44 +0300

    fix: changed firebaseConfig

--- src/api/firebaseApi.ts
+++ src/api/firebaseApi.ts
@@ -5,11 +5,11 @@ import {PostDataType} from '../redux/profile-reducer';
 const firebaseConfig = {
     apiKey: 'AIzaSyAiJY-qHhH7qhJM68K03AVA8QYOfrh8zAM',
     authDomain: 'connectify-c0124.firebaseapp.com',
+    databaseURL: 'https://connectify-c0124-default-rtdb.europe-west1.firebasedatabase.app',
     projectId: 'connectify-c0124',
     storageBucket: 'connectify-c0124.appspot.com',
     messagingSenderId: '574666849016',
     appId: '1:574666849016:web:3872f50d0b63a824b1590d',
-    databaseURL: 'https://connectify-c0124-default-rtdb.europe-west1.firebasedatabase.app/',
 };
 
 const app = initializeApp(firebaseConfig);
